a6d08b67aacbec72f5d015bab563a0e18bf61e4e
Ryan Chipman <ryan@ryanchipman.com>
2016-10-20 12:32:27 -0400

    Add evergreen wrapper script

---
+++ evg-notify.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+evg_notify_file=~/.evg-notify
+evg_notify_path=~/dev/evg-notify
+
+# if we are not submitting a patch, just pass through args to evergreen
+if [[ "$1" != "patch-file" && "$1" != "patch" ]]; then
+    evergreen $@
+    exit $?
+fi
+
+# log the output of the evergreen command to a file
+# without changing the user's interaction with the cmd
+script -q "$evg_notify_file" evergreen $@
+
+# get the patch ID from evergreen output
+patch_id=$(grep ID "$evg_notify_file" | awk '{print $3;}')
+
+# remove the temporary file
+rm "$evg_notify_file"
+
+nohup python $evg_notify_path/src/main.py $patch_id > /dev/null 2>&1 & disown
